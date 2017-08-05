9cad98313db597be422d8c8a74d4ea59739403a8
jptlightx005 <jptlightx005@gmail.com>
2017-08-05 10:44:48 +0800

    Implemented adding of symptoms

---
+++ public/library/dss/data/symptom.php
@@ -0,0 +1,34 @@
+<?php
+require_once('db.php');
+
+function getSymptomList(){
+	global $conn;
+	$query = "SELECT ID, name, description FROM dss_symptoms WHERE is_removed = 0";
+	return selectQuery($query);
+}
+
+function addNewSymptom($post){
+	global $conn;
+	$field_names = "(";
+	$field_values = "(";
+	foreach($post as $key => $value){
+		if($key != "action"){
+				$newValue = addslashes($value);
+				$field_names .= "`$key`, ";
+				$field_values .= "'$newValue', ";
+			}
+	}
+
+	$field_names = substr($field_names, 0, strlen($field_names) - 2) . ")";
+	$field_values = substr($field_values, 0, strlen($field_values) - 2) . ")";
+	
+	$query = "INSERT INTO `dss_symptoms` $field_names VALUES $field_values;";
+	
+	$stmt = $conn->prepare($query);
+	if($stmt->execute()){
+		return response(1, "Successfully added symptom!");
+	}else{
+		return response(0, "Operation failed!");
+	}
+}
+?>
--- public/views/dss.php
+++ public/views/dss.php
@@ -16,6 +16,7 @@ require_once(LIB_DIR . 'phpti-0.9/ti.php');
 
 require_once(LIB_DIR . 'dss/data/admin.php');
 require_once(LIB_DIR . 'dss/data/patient.php');
+require_once(LIB_DIR . 'dss/data/symptom.php');
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
