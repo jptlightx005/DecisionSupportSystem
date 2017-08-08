3e1bd101624e546bc22382205966768a5cb51490
jptlightx005 <jptlightx005@gmail.com>
2017-08-08 20:20:42 +0800

    Fixed searching in case

--- public/library/dss/data/case.php
+++ public/library/dss/data/case.php
@@ -5,7 +5,7 @@ function getCaseList($search){
 	global $conn;
 	$query = "SELECT dss_cases.ID, CaseID, dss_cases.PatientID,  dss_patients.first_name, dss_patients.middle_name, dss_patients.last_name, disease, diagnosis, treatment FROM dss_cases INNER JOIN dss_patients ON dss_cases.PatientID = dss_patients.ID WHERE dss_cases.is_removed = 0";
 	if($search != ""){
-		$query .= " AND (name LIKE '%$search%' OR diagnosis LIKE '%$search%' OR treatment LIKE '%$search%')";
+		$query .= " AND ((first_name LIKE '%$search%' OR middle_name LIKE '%$search%' OR last_name LIKE '%$search%') OR disease LIKE '%$search%' OR diagnosis LIKE '%$search%' OR treatment LIKE '%$search%')";
 	}
 	return selectQuery($query);
 }
